81498a34a57661ab33c83890e7fa458fa0793ba3
david <davidleon_06@hotmail.com>
2023-02-13 17:01:01 +0100

    Added expenses chart

--- my-expense-tracker/src/components/expenses/Expenses.jsx
+++ my-expense-tracker/src/components/expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import "../../styles/Expenses.css";
 import Card from "../Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -22,6 +23,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
